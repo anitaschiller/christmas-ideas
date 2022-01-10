843bc41d10d1c34203a9d78ff9e2dcc064dc7ed4
Anita Schiller <anita.s1@gmx.de>
2022-01-10 10:32:52 +0100

    Added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 
-import Card from './components/Card';
-
 function App() {
   const initialPersons = [
     { name: 'Mama', ideas: ['Mütze', 'Tasse'] },
@@ -10,9 +8,26 @@ function App() {
   ];
   const [personsToSendAGift, setPersonsToSendAGift] = useState(initialPersons);
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newPerson = event.target.newPerson.value;
+    const newIdeasString = event.target.newIdeas.value;
+    const newIdeasArray = newIdeasString.split(', ');
+    const newPersonObject = { name: newPerson, ideas: newIdeasArray };
+    setPersonsToSendAGift([...personsToSendAGift, newPersonObject]);
+    console.log(personsToSendAGift);
+  }
+
   return (
     <div className="App">
       <h1>Weihnachts-Ideen</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="newPerson">Add a new person</label>
+        <input type="text" id="newPerson" name="newPerson" />
+        <label htmlFor="newIdeas">Add your ideas</label>
+        <input type="text" id="newIdeas" name="newIdeas" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
